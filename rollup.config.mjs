bfe7e20f8b855b0e2a07d9dbe066923bda05214e
CalmDownVal <vaclav.lahuta@gmail.com>
2023-11-14 20:23:31 +0000

    setup repository and devtools

---
+++ rollup.config.mjs
@@ -0,0 +1,56 @@
+/* eslint-env node */
+/* eslint-disable import/no-default-export */
+
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import deleteBeforeBuild from 'rollup-plugin-delete';
+import serve from 'rollup-plugin-serve';
+
+const isDev = /^dev/i.test(process.env.ENV ?? '');
+const outDir = isDev
+	? './build/dev'
+	: './build/prod';
+
+const config = {
+	input: {
+		app: './src/app/index.js',
+		worklet: './src/worklet/index.js'
+	},
+	output: {
+		dir: outDir,
+		entryFileNames: '[name].min.js',
+		format: 'cjs',
+		sourcemap: isDev
+	},
+	plugins: [
+		deleteBeforeBuild({
+			runOnce: true,
+			targets: `${outDir}/*`
+		}),
+		nodeResolve({
+			browser: true,
+			preferBuiltins: false
+		}),
+		terser({
+			output: {
+				comments: false
+			}
+		})
+	]
+};
+
+if (/^true$/i.test(process.env.SERVER)) {
+	config.plugins.push(
+		serve({
+			contentBase: [
+				outDir,
+				'./static'
+			],
+			host: 'localhost',
+			open: true,
+			port: 8080
+		})
+	);
+}
+
+export default config;
